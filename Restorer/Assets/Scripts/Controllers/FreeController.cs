2d9f8e98fd466b916076bfa86dc73b0caa74f6a2
Igo_Go <chudakin98@gmail.com>
2021-03-04 12:35:08 +0700

    Падение в режиме свободного перемещения

--- Restorer/Assets/Scripts/Controllers/FreeController.cs
+++ Restorer/Assets/Scripts/Controllers/FreeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CharacterController))]
 public class FreeController : MonoBehaviour
 {
     [SerializeField]
@@ -11,13 +12,20 @@ public class FreeController : MonoBehaviour
     [SerializeField]
     private Animator anim;
 
+    [SerializeField]
+    private CharacterController controller;
+
     [SerializeField, Range(1,10)]
     private float moveSpeed = 1;
+    [SerializeField, Range(1, 10)]
+    private float fallAnimStartThreshold = 3;
+
+
 
     private Transform myTransform;
     private float animDepth = 0.1f;
-
-
+    private float maxVerSpeed = 40;
+    private float vertSpeed;
 
     void Start()
     {
@@ -42,8 +50,6 @@ public class FreeController : MonoBehaviour
 
         Vector3 dir = (camForward * ver + camRight * hor).normalized;
 
-        myTransform.position += dir * moveSpeed * Time.deltaTime;
-
         float move = Mathf.Clamp01(Mathf.Abs(ver) + Mathf.Abs(hor));
         if(move > 0.1f)
         {
@@ -54,5 +60,19 @@ public class FreeController : MonoBehaviour
         {
             anim.SetFloat("Locomotion", 0, 0.1f, Time.deltaTime);
         }
+
+        if (controller.isGrounded)
+        {
+            vertSpeed = 0;
+        }
+        else
+        {
+            vertSpeed += 9.8f * Time.deltaTime;
+        }
+
+        anim.SetBool("InAir", vertSpeed > fallAnimStartThreshold);
+
+        dir += Vector3.down * Mathf.Clamp(vertSpeed, 0, maxVerSpeed);
+        controller.Move(dir * moveSpeed * Time.deltaTime);
     }
 }
